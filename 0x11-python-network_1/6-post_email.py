113f1b9355e0be56182593c1861b0ca8a804c977
SOLOMONON <solomonodoki@yahoo.com>
2023-06-30 11:56:07 -0700

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -14,3 +14,16 @@ def main():
     if len(sys.argv) != 3:
         print("Usage: python3 script.py <URL> <email>")
         return
+
+    # Set up the POST request with the email as a parameter
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {"email": email}
+    response = requests.post(url, data=data)
+
+    # Display the body of the response
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
